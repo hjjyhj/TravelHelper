7b633ba98f3476b3835bd2d769ab6afc32d0c928
hjjyhj <95295245+hjjyhj@users.noreply.github.com>
2023-07-19 14:12:36 -0400

    12742

--- travel_helper/src/components/Map/Map.jsx
+++ travel_helper/src/components/Map/Map.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import GoogleMapReact from "google-map-react";
 import { Paper, Typography, useMediaQuery } from '@material-ui/core'
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-import Rationg from '@material-ui/lab'
+import Rating from '@material-ui/lab/Rating'
 import useStyles from './styles'
 
-const Map = ({ setCoordinates, setBounds, coordinates}) => {
+const Map = ({ setCoordinates, setBounds, coordinates, places}) => {
   const classes = useStyles();
-  const isMobile = useMediaQuery('(min-width:600px)');
+  const matches = useMediaQuery('(min-width:600px)');
 
   return (
     <div className={classes.mapContainer}>
@@ -25,7 +25,27 @@ const Map = ({ setCoordinates, setBounds, coordinates}) => {
         onChildClick = {''}
         
       >
-
+        {places?.map((place, i) => (
+          <div
+            className={classes.markerContainer}
+            lat={Number(place.latitude)}
+            lng={Number(place.longitude)}
+            key={i}
+          >
+            {!matches
+              ? <LocationOnOutlinedIcon color="primary" fontSize="large" />
+              : (
+                <Paper elevation={3} className={classes.paper}>
+                  <Typography className={classes.typography} variant="subtitle2" gutterBottom> {place.name}</Typography>
+                  <img
+                    className={classes.pointer}
+                    src={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                  />
+                  <Rating name="read-only" size="small" value={Number(place.rating)} readOnly />
+                </Paper>
+              )}
+          </div>
+        ))}
       </GoogleMapReact>
 
     </div>
