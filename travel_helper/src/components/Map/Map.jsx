6e564f4219679874d1ab08ae3d6be7c01ef218fd
hjjyhj <95295245+hjjyhj@users.noreply.github.com>
2023-07-26 07:52:00 -0400

    14631

--- travel_helper/src/components/Map/Map.jsx
+++ travel_helper/src/components/Map/Map.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import GoogleMapReact from "google-map-react";
 import { Paper, Typography, useMediaQuery } from '@material-ui/core'
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab/Rating'
 import useStyles from './styles'
 
-const Map = ({ setCoordinates, setBounds, coordinates, places}) => {
+const Map = ({ setCoordinates, setBounds, coordinates, places, setChildClicked}) => {
   const classes = useStyles();
   const matches = useMediaQuery('(min-width:600px)');
+  
 
   return (
     <div className={classes.mapContainer}>
@@ -22,7 +23,7 @@ const Map = ({ setCoordinates, setBounds, coordinates, places}) => {
           setCoordinates({ lat: e.center.lat, lng: e.center.lng})
           setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw })
         }}
-        onChildClick = {''}
+        onChildClick = {(child) => setChildClicked(child)}
         
       >
         {places?.map((place, i) => (
